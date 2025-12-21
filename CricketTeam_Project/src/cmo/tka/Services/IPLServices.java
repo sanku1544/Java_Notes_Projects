0ef219e92064223d4fa081de63c13f896fdc3815
Sanket N <sanketnikam1966@gmail.com>
2025-12-21 12:38:20 +0530

    update

--- CricketTeam_Project/src/cmo/tka/Services/IPLServices.java
+++ CricketTeam_Project/src/cmo/tka/Services/IPLServices.java
@@ -7,55 +7,70 @@ import com.rtka.database.Database;
 import com.tka.players.Players;
 
 public class IPLServices {
-	
-		Database db=new  Database();
-		
-		public List<Players> getPlayers(String teamn){
-		List<Players> motuList=db.getAllPlayers();
-		List<Players> chotuList= new ArrayList<Players>();
-		
-		for(Players p:motuList) {
-			if(p.getTname().equalsIgnoreCase(teamn)){
+
+	Database db = new Database();
+
+	public List<Players> getPlayers(String teamn) {
+		List<Players> motuList = db.getAllPlayers();
+		List<Players> chotuList = new ArrayList<Players>();
+
+		for (Players p : motuList) {
+			if (p.getTname().equalsIgnoreCase(teamn)) {
 				chotuList.add(p);
 			}
 		}
 		return chotuList;
-			
-		}
-		
-		public List<Players> getPlyersByCountry(String cname){
-			List<Players> motuList=db.getAllPlayers();
-			List<Players> chotuList= new ArrayList<Players>();
-			
-			for(Players p:motuList) {
-				if(p.getCname().equalsIgnoreCase(cname)) {
-					chotuList.add(p);
-				}
+
+	}
+
+	public List<Players> getPlyersByCountry(String cname) {
+		List<Players> motuList = db.getAllPlayers();
+		List<Players> chotuList = new ArrayList<Players>();
+
+		for (Players p : motuList) {
+			if (p.getCname().equalsIgnoreCase(cname)) {
+				chotuList.add(p);
 			}
-			return chotuList;
 		}
-		public List<Players> getPlyersByHighestRuns(int run){
-			List<Players> motuList=db.getAllPlayers();
-			List<Players> chotuList= new ArrayList<Players>();
-			
-			for(Players p:motuList) {
-				if(p.getRuns()>run) {
-					chotuList.add(p);
-				}
+		return chotuList;
+	}
+
+	public List<Players> getPlyersByHighestRuns(int run) {
+		List<Players> motuList = db.getAllPlayers();
+		List<Players> chotuList = new ArrayList<Players>();
+
+		for (Players p : motuList) {
+			if (p.getRuns() > run) {
+				chotuList.add(p);
 			}
-			return chotuList;
 		}
-		public List<Players> getPlyersTotalRuns(){
-			int totalRuns=0;
-			List<Players> motuList=db.getAllPlayers();
-			List<Players> chotuList= new ArrayList<Players>();
-			
-			for(Players p:motuList) {
-				totalRuns=totalRuns+p.getRuns();
-					
-				
+		return chotuList;
+	}
+
+	public List<Players> getPlyersTotalRuns() {
+		int totalRuns = 0;
+		List<Players> motuList = db.getAllPlayers();
+		List<Players> chotuList = new ArrayList<Players>();
+
+		for (Players p : motuList) {
+			totalRuns = totalRuns + p.getRuns();
+
+		}
+		System.out.println("Total runs: " + totalRuns);
+		return chotuList;
+	}
+
+	public List<Players> getPlyersStartWith(String n) {
+
+		List<Players> motuList = db.getAllPlayers();
+		List<Players> chotuList = new ArrayList<Players>();
+
+		for (Players p : motuList) {
+			if (p.getName().startsWith(n)) {
+				chotuList.add(p);
 			}
-			System.out.println("Total runs: "+totalRuns);
-			return chotuList;
 		}
+
+		return chotuList;
+	}
 }
--- CricketTeam_Project/src/com/tka/main/IPLControllrer.java
+++ CricketTeam_Project/src/com/tka/main/IPLControllrer.java
@@ -9,26 +9,32 @@ import cmo.tka.Services.IPLServices;
 public class IPLControllrer {
 
 	public static void main(String[] args) {
-		IPLServices ip= new IPLServices();
-		
-		List<Players> player1=ip.getPlayers("mi");
-		List<Players> player2=ip.getPlyersByCountry("South Africa");
-		List<Players> player3=ip.getPlyersByHighestRuns(4200);
-		List<Players> player4=ip.getPlyersTotalRuns();
+		IPLServices ip = new IPLServices();
+
+		List<Players> player1 = ip.getPlayers("mi");
+		List<Players> player2 = ip.getPlyersByCountry("South Africa");
+		List<Players> player3 = ip.getPlyersByHighestRuns(4200);
+		List<Players> player4 = ip.getPlyersTotalRuns();
+		List<Players> player5 = ip.getPlyersStartWith("Vi");
 		System.out.println("\nMi team players: ");
-		for(Players p:player1) {
-			System.out.println(p.getName()+" : "+p.getCname());
+		for (Players p : player1) {
+			System.out.println(p.getName() + " : " + p.getCname());
 		}
-		
+
 		System.out.println("\nSA players: ");
-		for(Players p1:player2) {
-			System.out.println(p1.getName()+" :- "+p1.getCname());
+		for (Players p1 : player2) {
+			System.out.println(p1.getName() + " :- " + p1.getCname());
 		}
+
 		System.out.println("\nPlayer runs greater than 4200 : ");
-		for(Players p1:player3) {
-			System.out.println(p1.getJn()+" :- "+ p1.getName()+" :- "+p1.getCname());
+		for (Players p1 : player3) {
+			System.out.println(p1.getJn() + " :- " + p1.getName() + " :- " + p1.getCname());
 		}
-		
+
+		System.out.println("\nPlayer name start with: ");
+		for (Players p1 : player5) {
+			System.out.println(p1.getName() + " :- " + p1.getCname());
+		}
 
 	}
 
